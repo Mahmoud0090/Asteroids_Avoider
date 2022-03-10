f398073f9bcdb4064c4c51867c738f8784e8f254
Mahmoud0090 <mmh.00090@gmail.com>
2022-03-11 00:26:26 +0300

    ship rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float forceMagnitude;
     [SerializeField] private float maxVelocity;
+    [SerializeField] private float rotationSpeed;
 
 
     private Rigidbody rb; 
@@ -27,6 +28,8 @@ public class PlayerMovement : MonoBehaviour
         processInput();
 
         KeepPlayerOnScreen();
+
+        RotateToFaceVelocity();
     }
 
     private void FixedUpdate()
@@ -85,4 +88,13 @@ public class PlayerMovement : MonoBehaviour
         transform.position = newPosition;
     }
 
+    private void RotateToFaceVelocity()
+    {
+        if(rb.velocity == Vector3.zero) { return; }
+
+        Quaternion targetRotation = Quaternion.LookRotation(rb.velocity, Vector3.back);
+
+        transform.rotation = Quaternion.Lerp
+            (transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
 }
